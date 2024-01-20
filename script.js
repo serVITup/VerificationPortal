a6fdaec73f44c1a7c41b4f7a9c15679e209d0776
abhishekkjainn <jainabhishek1904@gmail.com>
2024-01-20 21:46:44 +0530

    Basic Design Layout made

---
+++ script.js
@@ -0,0 +1,7 @@
+function resultsdiv() {
+  let resultsdiv = document.getElementById('result');
+  resultsdiv.style.height = '300px';
+  resultsdiv.style.marginBottom = '20px';
+  resultsdiv.style.transition = 'all 0.5s';
+  document.getElementById('backimage').style.transform = 'translateY(0)';
+}
